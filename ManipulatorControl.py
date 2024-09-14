298b12826678a38c3ecc27e324dd33325072cd35
Ryder Swan <ryderswan01@gmail.com>
2024-09-14 07:12:31 -0500

    File creation with outline comments

---
+++ ManipulatorControl.py
@@ -0,0 +1,34 @@
+# Robotic Arm Manipulator Program
+
+
+#Import Statements
+
+
+#Define Variables
+
+
+#Define Functions
+
+
+    #Connect to lynx motion controller
+
+
+    #Set Servo Positions
+    
+    
+    #Kinematic Calculations
+    
+    
+    #Reverse Kinematic Calculations
+    
+    
+#Main Loop
+
+
+#Define Waypoints
+
+
+#Run Functions
+
+
+#Output Positions
